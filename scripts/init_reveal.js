4dab018fb41c0dceefef06ba7c9bc7e03bba887b
segaviv <avivsegall@gmail.com>
2022-08-23 07:33:12 +0200

    Add external.js dependency, title slide.

--- scripts/init_reveal.js
+++ scripts/init_reveal.js
@@ -1,8 +1,10 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import {Reveald3} from '../plugin/reveald3'
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import RevealZoom from "reveal.js/plugin/zoom/zoom.esm";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm";
+import { Reveald3 } from "../plugin/reveald3";
 
 let deck = new Reveal({
-   plugins: [ Markdown, Reveald3 ]
-})
+  plugins: [Markdown, Reveald3, RevealZoom, RevealHighlight],
+});
 deck.initialize();
