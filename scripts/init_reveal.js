e6da285aacc69d248c7a9790c1fde16e5cfaa148
Aviv Segall <aviv.segall@inf.ethz.ch>
2022-08-25 16:52:54 +0200

    Use the latest reveal.js. The one form 4.3.1 has a bug in auto-animate.

--- scripts/init_reveal.js
+++ scripts/init_reveal.js
@@ -1,4 +1,4 @@
-import Reveal from "reveal.js";
+import Reveal from "./reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealZoom from "reveal.js/plugin/zoom/zoom.esm";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm";
@@ -6,6 +6,7 @@ import RevealMath from "reveal.js/plugin/math/math.esm";
 import { Reveald3 } from "../plugin/reveald3";
 
 let deck = new Reveal({
+  
   plugins: [RevealHighlight, Markdown, Reveald3, RevealZoom, RevealMath.KaTeX],
 });
 deck.initialize();
